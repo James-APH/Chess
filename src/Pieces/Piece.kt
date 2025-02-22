ad5e182eaf44082af6d53070f4f6471c57bf3650
James Huston <jamzhuston@gmail.com>
2025-02-22 13:00:51 -0700

    added folder for pieces, pieces will be in charge of checking for valid moves, displaying themselves in the correct color, and having the bool val inPlay

--- src/Pieces/Piece.kt
+++ src/Pieces/Piece.kt
@@ -1,1 +1,23 @@
-data class Piece(val type: String, val color: String = "blank", var display: String, var inPlay: Boolean = false)
+package Pieces
+
+open class Piece(private val color: String, private var position: Pair<Int, Int>, private var symbol: Char) {
+    constructor(color: String, symbol: Char) : this(color, Pair(0,0), symbol) {
+
+    }
+
+    fun setPosition(position: Pair<Int, Int>) {
+        this.position = position
+    }
+
+    fun getPosition(): Pair<Int, Int> {
+        return position
+    }
+
+    fun getSymbol(): Char {
+        return symbol
+    }
+
+    override fun toString(): String {
+        return "$color$symbol"
+    }
+}
