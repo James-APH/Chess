4d46901431b553267b62416e9a0bbe6c1026a041
James Huston <jamzhuston@gmail.com>
2025-02-22 15:36:25 -0700

    errors when testing

--- src/ChessGame.kt
+++ src/ChessGame.kt
@@ -2,36 +2,41 @@
  * Initializing white and black players with dependency injection
  * I will create the board, and piece sets in the constructor
  */
-class ChessGame(private var white: Player = Player(), private var black: Player = Player()) {
-    private var gameLogic: GameLogic = GameLogic()
-    private var board: Board = Board()
-    fun play() {
-        println("Would you like to play this game of chess? [y/n]")
-        var playGameAnswer: String = readln()
-        while (playGameAnswer.lowercase() != "n" && playGameAnswer.uppercase() != "no") {
-            var turn: String = "white"
-            while (gameLogic.isWinner(board) != true ) {
-                print(board.toString())
-                if (turn == "white") {
-                    white.playerTurn(board.getPieceList())
-                    turn = "black"
-                } else {
-                    black.playerTurn(board.getPieceList())
-                    turn = "white"
-                }
-                board.update(white, black)
-            }
-            var winner: String = gameLogic.getWinner()
-            if (winner == "black") {
-                black.winner()
-                white.loser()
-            } else if (winner == "white") {
-                white.winner()
-                black.loser()
-            }
-            println("Would you like to play again? [y/n]")
-            playGameAnswer = readln()
-        }
-        println("BYE!!!")
-    }
+class ChessGame() {
+
 }
+
+//class ChessGame(private var white: Player = Player(), private var black: Player = Player()) {
+//    private var gameLogic: GameLogic = GameLogic()
+//    private var board: Board = Board()
+//    fun play() {
+//        println("Would you like to play this game of chess? [y/n]")
+//        var playGameAnswer: String = readln()
+//        while (playGameAnswer.lowercase() != "n" && playGameAnswer.uppercase() != "no") {
+//            var turn: String = "white"
+//            while (gameLogic.isWinner(board) != true ) {
+//                print(board.toString())
+//                if (turn == "white") {
+//                    white.playerTurn(board.getPieceList())
+//
+//                    turn = "black"
+//                } else {
+//                    black.playerTurn(board.getPieceList())
+//                    turn = "white"
+//                }
+//                board.update(white, black)
+//            }
+//            var winner: String = gameLogic.getWinner()
+//            if (winner == "black") {
+//                black.winner()
+//                white.loser()
+//            } else if (winner == "white") {
+//                white.winner()
+//                black.loser()
+//            }
+//            println("Would you like to play again? [y/n]")
+//            playGameAnswer = readln()
+//        }
+//        println("BYE!!!")
+//    }
+//}
