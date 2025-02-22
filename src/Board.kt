7b0b076ab56f00a4445b3756a157abfd52b36687
James Huston <jamzhuston@gmail.com>
2025-02-22 15:35:26 -0700

    created board display function, and finished initialize board function (may change this to be the constructor

--- src/Board.kt
+++ src/Board.kt
@@ -8,22 +8,28 @@ class Board {
     fun initializeBoard() {
         for (i in 0..<rows) {
             for (j in 0..<cols) {
-                if (j + i % 2 == 1) {
-                    val piece: Piece = Piece("temp", "blank", "_", true)
+                if ((j + i) % 2 == 1) {
+                    val piece: Piece = Piece("\u001b[0m", Pair(i, j), '#')
                     board[i][j]=piece
                 } else {
-                    val piece: Piece = Piece("temp", "blank", "#", true)
+                    val piece: Piece = Piece("\u001B[31m", Pair(i, j), '#')
                     board[i][j]=piece
                 }
             }
         }
     }
 
-    override fun toString(): String {
-        return "Board(rows=$rows, cols=$cols)"
+    fun updateBoard(position: Pair<Int, Int>, newPosition: Pair<Int, Int>) {
+        // move the piece at position[i_1][j_1] to newPosition[i_2][j_2]
+        // replace position[i_1][j_1] with a new placeholder piece
     }
-    // have a actual board that we can loop through and display
-    // have two lists, one for black, and one for white, and let the players manipulate them
-    // still need to refine this idea. not completely sure how to track player moves.
-    // think I will track the overall board with a 2d array of pieces, need to have a default value for each piece.
+
+    fun displayBoard() {
+        for (i in 0..<rows) {
+            for (j in 0..<cols) {
+                print(board[i][j].toString() + " ")
+            }
+            println()
+        }
+    }
 }
